99a028b34faf5135098c869cf903c0b3f27710b3
Robin Singh <robinsinghdevgan@outlook.com>
2020-02-21 22:53:06 +0530

    updated 1_5 and added 1_7

--- ch1ArraysAndStrings/Ques1_5.java
+++ ch1ArraysAndStrings/Ques1_5.java
@@ -1,63 +1,70 @@
-public final class Ques1_5 {
-    public static int[][] rotateMatrix(int[][] A){
-        int n = A[0].length;
-        int[][] B = new int[n][n];
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n; ++j){
-                B[j][(n-1)-i] = A[i][j];
-            }
-        }
-        return B; 
+public class Ques1_5 {
+    public static void main(String[] args) {
+        String a = "pale";
+        String b = "bale";
+        System.out.println(oneAway(a, b)); 
     }
 
-    public static int[][] rotateMatrix2(int[][] A){
-        int n = A[0].length;
+    static boolean oneAway(String a, String b) {
+        if (a == "" || b == "")
+            return false;
+        if (a == b)
+            return true;
+        if (Math.abs(a.length() - b.length()) <= 1) {
+            System.out.println("here");
+            if (a.length() == b.length()){
+                if(oneReplacementAway(a,b))
+                    return true;
+            }
+            else if(oneInsertOrRemovalAway(a,b))
+                return true;
+        }
+        System.out.println("h3");
+        return false;
+    }
+
+    private static boolean oneInsertOrRemovalAway(String a, String b) {
+        System.out.println("oneInsertOrRemovalAway");
+        String smaller, larger;
+        if (a.length() > b.length()){
+            larger = a;
+            smaller = b;
+        }
+        else{
+            smaller = a;
+            larger = b;
+        }
+
+        boolean oneEdit = false;
+        for (int i = 0, j = 0, smallerStringLength = smaller.length();
+                i < smallerStringLength && j < smallerStringLength;
+                    ) {
+            if (smaller.charAt(i) == larger.charAt(j)){
+                ++i;++j;
+            }
+            else {
+                if (oneEdit)
+                    return false;
+                oneEdit = true;
+                ++j;
+            }            
+        }
+        return true;
+    }
+
+    private static boolean oneReplacementAway(String a, String b) {
+        //"if atleast length - 1 chars are same in both the strings"
+        System.out.println("oneReplacementAway");
         
-        for(int layer=0; layer<=n/2; ++layer){
-            //topLeft = A[layer][layer];
-            //topRight = A[layer][(n-1)-layer];
-            //bottomLeft = A[(n-1)-layer][layer];
-            //bottomRight = A[(n-1)-layer][(n-1)-layer];
-            int layerMinIndex = layer;
-            int layerMaxIndex = (n-1)-layer;
-            //traverse the layer to rotate elements at edges
-            for(int i = layerMinIndex; i < layerMaxIndex; ++i){
-                //temp = topLeft
-                int temp = A[layerMinIndex][layerMinIndex + i];
-                //topLeft = bottomLeft
-                A[layerMinIndex][layerMinIndex + i] = A[layerMaxIndex - i][layerMinIndex];
-                //bottomLeft = bottomRight
-                A[layerMaxIndex - i][layerMinIndex] = A[layerMaxIndex][layerMaxIndex - i];
-                //bottomRight = topRight
-                A[layerMaxIndex][layerMaxIndex - i] = A[layerMinIndex + i][layerMaxIndex];
-                //topRight = temp
-                A[layerMinIndex + i][layerMaxIndex] = temp; 
+        boolean oneEdit = false;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a.charAt(i) != b.charAt(i)) {
+                if (oneEdit)
+                    return false;
+                else
+                    oneEdit = true;
             }
         }
-        return A; 
+        return true;
     }
-
-    public static void printMatrix(int[][] A){
-        int n = A[0].length;
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n; ++j){
-                System.out.printf("%-4d ", A[i][j]);
-            }
-            System.out.println();
-        }
-    }
-  public static void main(String[] args) {
-      int[][] A = {{1,2,3,4,111},
-      {5,6,7,8,222},
-      {9,10,11,12,333},
-      {13,14,15,16,444},
-      {17,18,19,20,555}};
-      printMatrix(A);
-      int[][] B = rotateMatrix(A);
-      System.out.println("\n90 degrees right rotation using new matrix\n");
-      printMatrix(B);
-      B = rotateMatrix2(A);
-      System.out.println("\n90 degrees right rotation using in place\n");
-      printMatrix(B);
-  }
 }
